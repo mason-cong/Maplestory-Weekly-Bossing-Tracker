2c0ceff454381cb040b8310a1fe2b7d50d7bfc6f
Mason Cong <masoncong1@gmail.com>
2025-05-15 13:54:30 -0400

    added data classes

--- bossing/src/main/java/com/congmason/bossing/entity/WeeklyBoss.java
+++ bossing/src/main/java/com/congmason/bossing/entity/WeeklyBoss.java
@@ -1,10 +1,105 @@
 package com.congmason.bossing.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "weekly_bosses")
 public class WeeklyBoss {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bossing_id")
+    private Long id;
+
+    @Column(name = "boss_name", nullable = false)
+    private String bossName;
+
+    @Column(name = "party_size", nullable = false)
+    private String partySize;
+
+    @Column(name = "crystal_value", nullable = false)
+    private String crystalValue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "weekly_character_id")
+    private WeeklyCharacter weeklyCharacter;
+
+    public WeeklyBoss() {
+
+    }
+
+    public WeeklyBoss(Long id, String bossName, String partySize, String crystalValue, WeeklyCharacter weeklyCharacter) {
+        this.id = id;
+        this.bossName = bossName;
+        this.partySize = partySize;
+        this.crystalValue = crystalValue;
+        this.weeklyCharacter = weeklyCharacter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBossName() {
+        return bossName;
+    }
+
+    public void setBossName(String bossName) {
+        this.bossName = bossName;
+    }
+
+    public String getPartySize() {
+        return partySize;
+    }
+
+    public void setPartySize(String partySize) {
+        this.partySize = partySize;
+    }
+
+    public String getCrystalValue() {
+        return crystalValue;
+    }
+
+    public void setCrystalValue(String crystalValue) {
+        this.crystalValue = crystalValue;
+    }
+
+    public WeeklyCharacter getWeeklyCharacter() {
+        return weeklyCharacter;
+    }
+
+    public void setWeeklyCharacter(WeeklyCharacter weeklyCharacter) {
+        this.weeklyCharacter = weeklyCharacter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeeklyBoss that = (WeeklyBoss) o;
+        return Objects.equals(id, that.id) && Objects.equals(bossName, that.bossName) && Objects.equals(partySize, that.partySize) && Objects.equals(crystalValue, that.crystalValue) && Objects.equals(weeklyCharacter, that.weeklyCharacter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, bossName, partySize, crystalValue, weeklyCharacter);
+    }
+
+    @Override
+    public String toString() {
+        return "WeeklyBoss{" +
+                "id=" + id +
+                ", bossName='" + bossName + '\'' +
+                ", partySize='" + partySize + '\'' +
+                ", crystalValue='" + crystalValue + '\'' +
+                ", weeklyCharacter=" + weeklyCharacter +
+                '}';
+    }
 }
---
+++ bossing/src/main/java/com/congmason/bossing/entity/WeeklyCharacter.java
@@ -0,0 +1,120 @@
+package com.congmason.bossing.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "weekly_characters")
+public class WeeklyCharacter {
+
+    private final int BOSS_LIMIT = 14;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    private Long id;
+
+    @Column(name = "character_class", nullable = false)
+    private String characterClass;
+
+    @Column(name = "character_level", nullable = false)
+    private String characterLevel;
+
+    @Column(name = "character_name", nullable = false)
+    private String characterName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "weeklyCharacter", cascade = CascadeType.ALL, orphanRemoval = true) // Assuming the association is defined on the other entity
+    private List<WeeklyBoss> weeklyBosses;
+
+    public WeeklyCharacter() {
+    }
+
+    public WeeklyCharacter(Long id, String characterClass, String characterLevel, String characterName, User user, List<WeeklyBoss> weeklyBosses) {
+        this.id = id;
+        this.characterClass = characterClass;
+        this.characterLevel = characterLevel;
+        this.characterName = characterName;
+        this.user = user;
+        this.weeklyBosses = weeklyBosses;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public String getCharacterLevel() {
+        return characterLevel;
+    }
+
+    public void setCharacterLevel(String characterLevel) {
+        this.characterLevel = characterLevel;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<WeeklyBoss> getWeeklyBosses() {
+        return weeklyBosses;
+    }
+
+    public void setWeeklyBosses(List<WeeklyBoss> weeklyBosses) {
+        this.weeklyBosses = weeklyBosses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeeklyCharacter that = (WeeklyCharacter) o;
+        return Objects.equals(id, that.id) && Objects.equals(characterClass, that.characterClass) && Objects.equals(characterLevel, that.characterLevel) && Objects.equals(characterName, that.characterName) && Objects.equals(user, that.user) && Objects.equals(weeklyBosses, that.weeklyBosses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, characterClass, characterLevel, characterName, user, weeklyBosses);
+    }
+
+    @Override
+    public String toString() {
+        return "WeeklyCharacter{" +
+                "id=" + id +
+                ", characterClass='" + characterClass + '\'' +
+                ", characterLevel='" + characterLevel + '\'' +
+                ", characterName='" + characterName + '\'' +
+                ", user=" + user +
+                ", weeklyBosses=" + weeklyBosses +
+                '}';
+    }
+}
+
